c7c9b86f7218485ba179101d10fab9fc64a57ec7
yarkob <yaroslav.klark.bondarenko@gmail.com>
2024-12-30 12:13:05 +0200

    improve title styles and margins

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -6,6 +6,8 @@ import { Product } from '../../types';
 import { Pagination } from '../Pagination';
 import { Sorts } from '../Sorts';
 
+import s from './Catalog.module.scss';
+
 interface Props {
   title: string;
   products: Product[];
@@ -38,9 +40,9 @@ export const Catalog: FC<Props> = ({ title, products }) => {
 
   return (
     <div>
-      <div>
-        <h1>{title}</h1>
-        <p>{products.length}</p>
+      <div className={s.titleContainer}>
+        <h1 className={s.title}>{title}</h1>
+        <p className={s.numberOfModels}>{products.length} models</p>
       </div>
       <Sorts />
       <Pagination products={sortedProducts} />
