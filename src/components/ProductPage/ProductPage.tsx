2fa89de63a18a7d8aae15f2ded643923a50000a1
yarkob <yaroslav.klark.bondarenko@gmail.com>
2025-01-02 11:57:50 +0200

    add basic grid layout

--- src/components/ProductPage/ProductPage.tsx
+++ src/components/ProductPage/ProductPage.tsx
@@ -1,6 +1,10 @@
 import { FC } from 'react';
 
+import { Variants } from '../../constants';
 import { Product } from '../../types';
+import Button from '../ui/Button';
+
+import s from './ProductPage.module.scss';
 
 interface Props {
   product: Product | undefined;
@@ -15,7 +19,9 @@ export const ProductPage: FC<Props> = ({ product }) => {
 
   return (
     <div>
-      <h2>{name}</h2>
+      <Button variant={Variants.Back} />
+      <h2 className={s.name}>{name}</h2>
+      <div className={s.content}></div>
     </div>
   );
 };
