1fe558c50b4765209a0ca4a29371de211c524efd
yarkob <yaroslav.klark.bondarenko@gmail.com>
2025-01-03 11:43:26 +0200

    fix ProductSlider and a bug with Link

--- src/components/ui/ProductCard/ProductCard.tsx
+++ src/components/ui/ProductCard/ProductCard.tsx
@@ -26,52 +26,54 @@ export const ProductCard: FC<Props> = ({ product }) => {
   };
 
   return (
-    <Link to={`/${product.category}/${product.itemId}`} className={s.container}>
-      <div className={s.content}>
-        <img className={s.image} alt="phone" src={`./${product.image}`} />
-        <p title={product.name} className={s.name}>
-          {product.name}
-        </p>
-        <div className={s.price}>
-          <span className={s.priceDiscount}>${product.price}</span>
-          <span className={s.priceRegular}>${product.fullPrice}</span>
-        </div>
-        <span className={s.line} />
-        <div className={s.specs}>
-          <div className={s.spec}>
-            <span className={s.specName}>Screen</span>
-            <span className={s.specValue}>{product.screen}</span>
+    <Link to={`/${product.category}/${product.itemId}`}>
+      <div className={s.container}>
+        <div className={s.content}>
+          <img className={s.image} alt="phone" src={`./${product.image}`} />
+          <p title={product.name} className={s.name}>
+            {product.name}
+          </p>
+          <div className={s.price}>
+            <span className={s.priceDiscount}>${product.price}</span>
+            <span className={s.priceRegular}>${product.fullPrice}</span>
           </div>
-          <div className={s.spec}>
-            <span className={s.specName}>Capacity</span>
-            <span className={s.specValue}>{product.capacity}</span>
+          <span className={s.line} />
+          <div className={s.specs}>
+            <div className={s.spec}>
+              <span className={s.specName}>Screen</span>
+              <span className={s.specValue}>{product.screen}</span>
+            </div>
+            <div className={s.spec}>
+              <span className={s.specName}>Capacity</span>
+              <span className={s.specValue}>{product.capacity}</span>
+            </div>
+            <div className={s.spec}>
+              <span className={s.specName}>RAM</span>
+              <span className={s.specValue}>{product.ram}</span>
+            </div>
           </div>
-          <div className={s.spec}>
-            <span className={s.specName}>RAM</span>
-            <span className={s.specValue}>{product.ram}</span>
+          <div className={s.buttons}>
+            <Button
+              onClick={addToCartHandler}
+              isSelected={isSelected}
+              variant={Variants.Primary}
+            >
+              {isSelected ? 'Added' : 'Add to cart'}
+            </Button>
+            <Button className={s.favorite} variant={Variants.Favorites}>
+              {isFavorite ? (
+                <Icon
+                  onClick={favoriteHandler}
+                  iconId={Icons.FavoritesFilled}
+                  className={cn('', {
+                    [s.filled]: isFavorite,
+                  })}
+                />
+              ) : (
+                <Icon iconId={Icons.Favorites} onClick={favoriteHandler} />
+              )}
+            </Button>
           </div>
-        </div>
-        <div className={s.buttons}>
-          <Button
-            onClick={addToCartHandler}
-            isSelected={isSelected}
-            variant={Variants.Primary}
-          >
-            {isSelected ? 'Added' : 'Add to cart'}
-          </Button>
-          <Button className={s.favorite} variant={Variants.Favorites}>
-            {isFavorite ? (
-              <Icon
-                onClick={favoriteHandler}
-                iconId={Icons.FavoritesFilled}
-                className={cn('', {
-                  [s.filled]: isFavorite,
-                })}
-              />
-            ) : (
-              <Icon iconId={Icons.Favorites} onClick={favoriteHandler} />
-            )}
-          </Button>
         </div>
       </div>
     </Link>
