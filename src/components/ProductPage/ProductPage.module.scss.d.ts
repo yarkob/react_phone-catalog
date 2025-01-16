36fd8e932fb00160826f155bffbe06778f5e70a0
yarkob <yaroslav.klark.bondarenko@gmail.com>
2025-01-16 16:48:22 +0200

    add unversioned file

--- src/components/ProductPage/ProductPage.module.scss.d.ts
+++ src/components/ProductPage/ProductPage.module.scss.d.ts
@@ -2,3 +2,4 @@ export declare const backButton: string;
 export declare const content: string;
 export declare const name: string;
 export declare const slider: string;
+export declare const variants: string;
